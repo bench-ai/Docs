8296d08ca56692a688a829fa057e76a79da8dfc0
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2023-07-16 01:53:00 -0400

    Add Preview link and Update Intro

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,20 +3,21 @@ import {DocsThemeConfig, useConfig} from 'nextra-theme-docs'
 import {useRouter} from "next/router";
 
 const config: DocsThemeConfig = {
-    logo: <span style={{display: "flex", alignItems: "center", gap: "10px"}}>
-    <svg width="32" height="32" viewBox="0 0 65 42" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect y="27.5171" width="65" height="7.96552" fill="#7E6000"/>
-        <rect x="11.6853" y="35.4829" width="4.38202" height="6.51724" fill="#7E6000"/>
-        <rect x="48.9326" y="35.4829" width="4.38202" height="6.51724" fill="#7E6000"/>
-        <rect x="9.49438" y="13.7588" width="46.0112" height="7.24138" fill="#7E6000"/>
-        <rect x="9.49438" width="46.0112" height="7.24138" fill="#7E6000"/>
-    </svg>
-    Bench AI Docs
-</span>,
+    logo:
+        <span style={{display: "flex", alignItems: "center", gap: "10px"}}>
+            <svg width="32" height="32" viewBox="0 0 65 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect y="27.5171" width="65" height="7.96552" fill="#7E6000"/>
+                <rect x="11.6853" y="35.4829" width="4.38202" height="6.51724" fill="#7E6000"/>
+                <rect x="48.9326" y="35.4829" width="4.38202" height="6.51724" fill="#7E6000"/>
+                <rect x="9.49438" y="13.7588" width="46.0112" height="7.24138" fill="#7E6000"/>
+                <rect x="9.49438" width="46.0112" height="7.24138" fill="#7E6000"/>
+            </svg>
+            Bench AI Docs
+        </span>,
     project: {
         link: 'https://github.com/Bench-ai',
     },
-    docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+    docsRepositoryBase: 'https://github.com/Bench-ai/Docs',
     useNextSeoProps() {
         const {asPath} = useRouter()
         if (asPath !== '/') {
@@ -28,8 +29,6 @@ const config: DocsThemeConfig = {
     head: function useHead() {
         const {title} = useConfig()
         const {route} = useRouter()
-        const socialCard =
-            route === '/' || 'https://bench-ai.com';
 
         return (
             <>
@@ -39,22 +38,22 @@ const config: DocsThemeConfig = {
                 <meta httpEquiv="Content-Language" content="en"/>
                 <meta
                     name="description"
-                    content="Make beautiful websites with Next.js & MDX."
+                    content="Bench AI Docs. Train and Deploy your machine learning models on the cloud fast and simple."
                 />
                 <meta
                     name="og:description"
-                    content="Make beautiful websites with Next.js & MDX."
+                    content="Bench AI Docs. Train and Deploy your machine learning models on the cloud fast and simple."
                 />
                 <meta name="twitter:card" content="summary_large_image"/>
-                {/*<meta name="twitter:image" content={socialCard} />*/}
+                <meta name="twitter:image" content="/preview.png" />
                 <meta name="twitter:site:domain" content="https://bench-ai.com"/>
                 <meta name="twitter:url" content="https://bench-ai.com"/>
                 <meta
                     name="og:title"
                     content={title ? title + ' – Bench AI Docs' : 'Bench AI Docs'}
                 />
-                {/*<meta name="og:image" content={socialCard} />*/}
-                <meta name="apple-mobile-web-app-title" content="Nextra"/>
+                <meta name="og:image" content="/preview.png" />
+                <meta name="apple-mobile-web-app-title" content="Bench AI"/>
                 <link rel="icon" href="/favicon.svg" type="image/svg+xml"/>
                 <link rel="icon" href="/favicon.png" type="image/png"/>
                 <link
@@ -73,7 +72,7 @@ const config: DocsThemeConfig = {
         )
     },
     banner: {
-        key: '2.0-release',
+        key: 'beta-release',
         text: (
             <a href="https://forms.gle/3HDGxeQxJt9CHK9Q9" target="_blank" rel="noreferrer">
                 🎉 Bench AI Beta is Released. Join the Waitlist →
